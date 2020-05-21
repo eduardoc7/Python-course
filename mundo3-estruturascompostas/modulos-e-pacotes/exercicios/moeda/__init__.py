bf989e990dccbdb87e917d60ae18bf6a4879155b
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-05-21 03:17:34 -0300

    creation exe107

---
+++ mundo3-estruturascompostas/modulos-e-pacotes/exercicios/moeda/__init__.py
@@ -0,0 +1,17 @@
+def metade(preco):
+    return preco / 2
+
+
+def dobro(preco):
+    return preco * 2
+
+
+def aumentar(preco):
+    p = (preco * 10) / 100
+    return preco + p
+
+
+def reduzindo(preco):
+    p = (preco * 13) / 100
+    return preco - p
+
